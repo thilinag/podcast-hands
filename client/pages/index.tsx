80f96d7a2bc1a746fceac81bbe8a345be53c9ccc
Thilina <thilina@aligent.com.au>
2020-07-01 15:41:20 +0930

    save username in localstorage because we are lazy to type again and again 	* use a real form so we can submit by hitting enter 	* make username field required 	* style the form

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,24 +1,41 @@
 import Layout from '../components/Layout'
 import dynamic from 'next/dynamic'
-import { useState, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import SessionView from '../components/SessionView';
+import styles from "./index.module.css";
+
+const StartSessionView = (props) => {
+  const { nameRef, setSessionState } = props;
+  const [userName, setUserName] = useState<String>('');
+
+  useEffect(() => {
+    const savedUserName = localStorage.getItem('userName');
+    if (savedUserName) {
+      setUserName(savedUserName);
+    }
+  });
+
+  const handleChange = (e) => {
+    localStorage.setItem('userName', e.target.value);
+    setUserName(e.target.value);
+  }
+
+  return (<form className={styles.joinForm} onSubmit={() => setSessionState(true)}>
+    <input required placeholder='Name' value={userName} onChange={handleChange} className={styles.nameInput} type="text" name={"screenName"} ref={nameRef} />
+    <button className={styles.joinButton}>Join</button>
+  </form>);
+};
 
 const IndexPage = () => {
 
   const nameRef = useRef<HTMLInputElement>(null);
   const [sessionStatus, setSessionState] = useState<boolean>(false);
-
-  const StartSessionView = () => (
-    <div>
-      <input type="text" name={"screenName"} ref={nameRef} />
-      <button onClick={() => setSessionState(true)}>Connect</button>
-    </div>
-  )
-
-
+  
   return (
     <Layout title="TK Hands APP">
-      {!sessionStatus? <StartSessionView/> : <SessionView name={nameRef.current?.value || ""}/>}
+      {!sessionStatus 
+        ? <StartSessionView nameRef={nameRef} setSessionState={setSessionState} /> 
+        : <SessionView name={nameRef.current?.value || ""} />}
     </Layout>
   )
 }
