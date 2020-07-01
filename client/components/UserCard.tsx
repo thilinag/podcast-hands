bccb7e202fdb1e29d631e0aee0c13ded39b3dd06
Thilina <thilina@aligent.com.au>
2020-07-01 17:52:07 +0930

    add a count up after usser setting wantsToTalk

--- client/components/UserCard.tsx
+++ client/components/UserCard.tsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from "./UserCard.module.css";
 import User from '../interfaces'
 
 const UserCard = (props: User) => {
 
-    const { user: { name, wantsToTalk }} = props;
+    const { user: { name, wantsToTalk, queuedAt }} = props;
     const emoji:string = wantsToTalk ? '✋' : '';
-    
+    const [ elapsedSeconds, setElapsedSeconds ] = useState<number>(0);
+
+    const calculateElapsedSeconds = () => {
+        return Math.floor((new Date() - new Date(queuedAt)) / 1000);
+    }
+
+    useEffect(() => {
+      setTimeout(() => {
+        setElapsedSeconds(calculateElapsedSeconds());
+      }, 1000);
+    });
+
     return (
         <div className={styles.user}>
             {name}
             {wantsToTalk && 
-                <i className={styles.userStatus}>{ emoji }</i>
+                <span className={styles.userStatus}>
+                    <span className={styles.userStatusIcon}>
+                        { emoji } 
+                    </span>
+                    <small className={styles.ellapsedSeconds}>
+                        {elapsedSeconds > 0 ? elapsedSeconds: ''}
+                    </small>
+                </span>
             }
         </div>
     )
